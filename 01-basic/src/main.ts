681d81001d682dee1dea64aa75478cf1d5fdccb1
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-28 01:04:09 +0800

    :sparkles: Init project
    Add basic 01 for nestjs

---
+++ 01-basic/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+bootstrap().catch(Logger.error);
